de529c7afca7932df149bebbd9f511455f8e7f29
Zenglijun <june_z@163.com>
2025-02-17 15:13:59 +0800

    new network file

--- 3. differential gut microbiota/3_2_network.R
+++ 3. differential gut microbiota/3_2_network.R
@@ -1,36 +1,38 @@
 rm(list = ls())
-pacman::p_load(dplyr, ggplot2, vegan, rstudioapi, WGCNA, psych, reshape2, igraph)
+pacman::p_load(dplyr, ggplot2, vegan, rstudioapi, igraph)
 setwd(dirname(getActiveDocumentContext()$path))
+library(psych)
+library(reshape2)
 
-otu_table <- read.csv("otu_abs.csv",header=T,row.names=1)
-sp <- read.csv("masslin_res.csv",header=T)
+otu_table <- read.csv("../otu_abs.csv", header=T, row.names=1)
+sp <- read.csv("../masslin_res.csv", header=T)
 otu_table <- otu_table %>% filter(row.names(otu_table) %in% sp$Species)
+write.table(otu_table, file = "otu_abs.tsv", row.names=T, sep ="\t", quote = F, col.names = F)
 
-rel_abundance <- apply(otu_table, 2, function(x) x/sum(x))
-mean_rel_abundance <- rowMeans(rel_abundance) 
-low_rel_abundance_otu <- rownames(otu_table)[mean_rel_abundance < 0.0001]
-otu_table_filtered <- otu_table[!(rownames(otu_table) %in% low_rel_abundance_otu), ]
-freq <- apply(otu_table_filtered, 1, function(x) sum(x > 0)/length(x))
-keep <- freq >= 1/5
-otu_table_filt <- otu_table_filtered[keep, ]
-write.csv(otu_table_filt, 'otu.csv')
+cor_sparcc <- read.delim('cor_mat_sparcc_iter5_.out', row.names = 1, sep = '\t', check.names = FALSE)
+pvals <- read.delim('pvals.two_sided_.txt', row.names = 1, sep = '\t', check.names = FALSE)
 
-otu <- read.csv('otu.csv', header = T, row.names = 1)
-cor <- corAndPvalue(t(otu),y=NULL,use = "pairwise.complete.obs", alternative='two.sided',method='spearman')
+cor_sparcc[abs(cor_sparcc) < 0.3] <- 0
+write.csv(as.data.frame(cor_sparcc), 'cor_sparcc.csv')
 
-r <- cor$cor
-p <- cor$p
-p <- p.adjust(p, method = 'BH')
-r[p > 0.001 | abs(r) < 0.60] = 0
-write.csv(data.frame(r, check.names = FALSE), 'corr.matrix.csv')
-g = graph_from_adjacency_matrix(r,mode="undirected",weighted=TRUE,diag = FALSE)
-V(g)$Label <- rownames(r)
+pvals[pvals>=0.05] <- -1
+pvals[pvals<0.05 & pvals>=0] <- 1
+pvals[pvals==-1] <- 0
 
-g = delete.vertices(g, names(degree(g)[degree(g) == 0]))
-E(g)$corr = E(g)$weight
-E(g)$weight = E(g)$weight
+adj <- as.matrix(cor_sparcc) * as.matrix(pvals)
+diag(adj) <- 0
+write.table(data.frame(adj, check.names = FALSE), 'neetwork.adj.txt', col.names = NA, sep = '\t', quote = FALSE)
 
-tax = read.csv('otu_tax.csv', row.names=1, header=T)
+network_adj <- read.delim('neetwork.adj.txt', row.names = 1, sep = '\t', check.names = FALSE)
+
+g <- graph_from_adjacency_matrix(as.matrix(network_adj), mode = 'undirected', weighted = TRUE, diag = FALSE)
+g <- delete_vertices(g, names(degree(g)[degree(g) == 0]))
+g
+
+E(g)$sparcc <- E(g)$weight
+E(g)$weight <- abs(E(g)$weight)
+
+tax = read.csv('../otu_tax.csv', row.names=1, header=T)
 tax = tax[as.character(V(g)$name), ]
 V(g)$Kingdom = tax$kingdom
 V(g)$Phylum = tax$phyla
@@ -40,25 +42,19 @@ V(g)$Family = tax$family
 V(g)$Genus = tax$genus
 V(g)$Species = tax$species
 
-node_list = data.frame(  
-  label = names(V(g)),  
-  kingdom = V(g)$Kingdom,  
-  phylum = V(g)$Phylum,  
-  class = V(g)$Class, 
-  order = V(g)$Order, 
-  family = V(g)$Family, 
-  genus=V(g)$Genus, 
-  species = V(g)$Species)
-head(node_list)
-write.csv(node_list, 'network.node_list.csv')
+write_graph(g, 'network.graphml', format = 'graphml')
 
-edge = data.frame(as_edgelist(g))
-edge_list = data.frame(  
-  source = edge[[1]],  
-  target = edge[[2]],  
-  weight = E(g)$weight,  
-  correlation = E(g)$corr)
-head(edge_list)
-write.csv(edge_list, 'network.edge_list.csv')
+edge <- data.frame(as_edgelist(g))
+edge_list <- data.frame(
+  source = edge[[1]],
+  target = edge[[2]],
+  weight = E(g)$weight,
+  sparcc = E(g)$sparcc
+)
+write.table(edge_list, 'network.edge_list.txt', sep = '\t', row.names = FALSE, quote = FALSE)
 
-write.graph(g, 'network.graphml', format = 'graphml')
+node_list <- data.frame(
+  nodes_id = V(g)$name,
+  degree = degree(g)
+)
+write.table(node_list, 'network.node_list.txt', sep = '\t', row.names = FALSE, quote = FALSE)
